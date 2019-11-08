7afab2ecdca8ac8635ce51bf2d8a438592f10d3e
Flavio Odas <odasflavio@hotmail.com>
2019-11-07 21:47:19 -0300

    estrutura de telas de cadastro e configuracao de conta

---
+++ desk/Desktop_View/ConfiguracoesCadastro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_View
+{
+    public partial class ConfiguracoesCadastro : MaterialSkin.Controls.MaterialForm
+    {
+        public ConfiguracoesCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfiguracoesCadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
