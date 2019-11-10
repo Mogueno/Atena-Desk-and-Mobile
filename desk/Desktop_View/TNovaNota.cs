ef79087ad7a5bd28a2f30edf96d09e20ab09b364
Flavio <flavio.odas@acct.global>
2019-11-10 14:24:34 -0300

    adição de arquivos filtrados

---
+++ desk/Desktop_View/TNovaNota.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_View
+{
+    public partial class TNovaNota : MaterialSkin.Controls.MaterialForm
+    {
+        public TNovaNota()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TNovaNota_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
